cbc643cc33836e31aafdc6a1b62f821e22d7a341
Oleksandr Akhobadze <sandoahobadze@gmail.com>
2024-12-03 11:28:31 +0100

    adding tests to test_main.py

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,18 @@
 from app.main import is_isogram
 
-# write your code here
+
+def test_with_empty_string() -> None:
+    assert is_isogram("") is True
+
+
+def test_if_program_is_case_consecutive() -> None:
+    word = "Adam"
+    assert is_isogram(word) is False
+
+
+def test_with_duplicates() -> None:
+    assert is_isogram("look") is False
+
+
+def test_with_only_upper_letters() -> None:
+    assert is_isogram("ABCVSDA") is False
